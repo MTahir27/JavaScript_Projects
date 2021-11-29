d8b648b27a2c661c8ff2b322cea9a22cd387517f
Muhammad Tahir <m.tahiridrees27@gmail.com>
2021-11-29 20:05:55 +0500

    Project 1 Link Added

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>JavaScript Projects</title>
+</head>
+
+<body>
+    <h1>Here is the JavaScript Project Links:</h1>
+    <ul>
+        <li>
+            <a href="1-Color_Fliper/index.html">Color Filper</a>
+        </li>
+    </ul>
+</body>
+
+</html>
