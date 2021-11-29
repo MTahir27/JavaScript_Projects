5b8fde135b037f99819de339955e0d674fa026a8
Muhammad Tahir <m.tahiridrees27@gmail.com>
2021-11-29 22:23:53 +0500

    Hex Color Code Generated

--- 1-Color_Fliper/script.js
+++ 1-Color_Fliper/script.js
@@ -1,16 +1,33 @@
+// Color Name Array
 const bgColor = ['red', 'green', 'blue', 'pink'];
-
+// Hex Color Array
+const bgHexColor = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'];
+// MainSection Div Selected by ID
 const mainSection = document.getElementById('mainSection');
-
-const colorName = document.getElementById('colorName');
-
-const bgChangeBtn = document.getElementById('bgChangeBtn');
+// Color Name Show span Select by ID
 const color = document.getElementById('color');
+// Color Type Name Selected By ID
+const colorName = document.getElementById('colorName');
+// Hex Color Type Selected by ID
+const hexColor = document.getElementById('hexColor');
+// Change Color Button Selected by ID
+const bgChangeBtn = document.getElementById('bgChangeBtn');
+
 
 bgChangeBtn.addEventListener("click", function () {
-    totalColor = bgColor.length;
-    const colorIndex = Math.floor(Math.random() * totalColor);
+    // totalColor = bgColor.length;
+    // const colorIndex = Math.floor(Math.random() * totalColor);
+    // mainSection.style.background = bgColor[colorIndex];
+    // color.innerText = bgColor[colorIndex];
 
-    mainSection.style.background = bgColor[colorIndex];
-    color.innerText = bgColor[colorIndex];
+    totalHexColor = bgHexColor.length;
+    hexColorName = Math.floor(Math.random() * totalHexColor);
+    let hex = '#';
+    let i = 0;
+    for (i = 0; i < 6; i++) {
+        hexColorIndex = Math.floor(Math.random() * totalHexColor);
+        hex = hex + bgHexColor[hexColorIndex];
+    }
+    mainSection.style.background = hex;
+    color.innerText = hex;
 });
