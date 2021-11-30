8b904a6ce6614a42a039c0c6829a1e428456c014
Muhammad Tahir <m.tahiridrees27@gmail.com>
2021-11-30 22:56:56 +0500

    Client Review Store in Array

---
+++ 3-Reviews_Carousel/script.js
@@ -0,0 +1,42 @@
+const clientReviews = [{
+        name: "John Deo",
+        job: "Front-end Web Developer",
+        image: "images/Agent-1.png",
+        message: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque aliquam cumque corrupti beatae illum non quae fugiat facilis aut modi nihil, maiores inventore laborum perferendis distinctio.Nisi aliquid repudiandae iure!"
+    },
+
+    {
+        name: "Sabrina Green",
+        job: "UI/UX designer",
+        image: "images/Agent-2.png",
+        message: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque aliquam cumque corrupti beatae illum non quae fugiat facilis aut modi nihil, maiores inventore laborum perferendis distinctio.Nisi aliquid repudiandae iure!"
+    },
+
+    {
+        name: "Gwen Garza",
+        job: "Baxk-end Web Developer",
+        image: "images/Agent-3.png",
+        message: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque aliquam cumque corrupti beatae illum non quae fugiat facilis aut modi nihil, maiores inventore laborum perferendis distinctio.Nisi aliquid repudiandae iure!"
+    },
+
+    {
+        name: "Kristi Larson",
+        job: "React Developer",
+        image: "images/Agent-4.png",
+        message: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque aliquam cumque corrupti beatae illum non quae fugiat facilis aut modi nihil, maiores inventore laborum perferendis distinctio.Nisi aliquid repudiandae iure!"
+    },
+
+    {
+        name: "Pauline Mckenzie",
+        job: "Node JS Developer",
+        image: "images/Agent-5.png",
+        message: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque aliquam cumque corrupti beatae illum non quae fugiat facilis aut modi nihil, maiores inventore laborum perferendis distinctio.Nisi aliquid repudiandae iure!"
+    },
+
+    {
+        name: "Genevieve Edwards",
+        job: "Graphic Designer",
+        image: "images/Agent-6.png",
+        message: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque aliquam cumque corrupti beatae illum non quae fugiat facilis aut modi nihil, maiores inventore laborum perferendis distinctio.Nisi aliquid repudiandae iure!"
+    },
+];
