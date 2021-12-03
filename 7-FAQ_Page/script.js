d7d91ef639076e7fa431519b5cfde413f19d61fd
Muhammad Tahir <m.tahiridrees27@gmail.com>
2021-12-04 01:39:02 +0500

    Toggle only click on icon

--- 7-FAQ_Page/script.js
+++ 7-FAQ_Page/script.js
@@ -5,10 +5,10 @@ const faqs = document.querySelectorAll('.faq');
 faqs.forEach(function (faq) {
     faq.addEventListener('mouseenter', function (e) {
         // Select Faq Icon
-        const faqQuestion = e.currentTarget;
+        const faq = e.currentTarget;
         const faqImg = faq.childNodes['1'].childNodes['3'];
         // On CLick Show FAQ COntent
-        faqQuestion.onclick = function () {
+        faqImg.onclick = function () {
             faq.classList.toggle('show');
             // Change Image of FAQ
             if (faqImg.src.match("plus.png")) {
