e9d96c6c9d848e75738a76809a7e9c9fc1c72ef1
Muhammad Tahir <m.tahiridrees27@gmail.com>
2021-12-04 01:36:49 +0500

    Name Change of Constant

--- 7-FAQ_Page/script.js
+++ 7-FAQ_Page/script.js
@@ -5,9 +5,10 @@ const faqs = document.querySelectorAll('.faq');
 faqs.forEach(function (faq) {
     faq.addEventListener('mouseenter', function (e) {
         // Select Faq Icon
+        const faqQuestion = e.currentTarget;
         const faqImg = faq.childNodes['1'].childNodes['3'];
         // On CLick Show FAQ COntent
-        faqImg.onclick = function () {
+        faqQuestion.onclick = function () {
             faq.classList.toggle('show');
             // Change Image of FAQ
             if (faqImg.src.match("plus.png")) {
