0f1ad331b0b4e718497458e39768fa7fe630275a
Muhammad Tahir <m.tahiridrees27@gmail.com>
2021-12-04 01:02:11 +0500

    Review Image Url Change

--- 3-Reviews_Carousel/script.js
+++ 3-Reviews_Carousel/script.js
@@ -2,7 +2,8 @@ const clientReviews = [{
         id: 1,
         name: "John Deo",
         job: "Front-end Web Developer",
-        image: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+        // image: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+        image: "images/Agent-1.png",
         message: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque aliquam cumque corrupti beatae illum non quae fugiat facilis aut modi nihil, maiores inventore laborum perferendis distinctio.Nisi aliquid repudiandae iure!"
     },
 
@@ -10,7 +11,8 @@ const clientReviews = [{
         id: 2,
         name: "Sabrina Green",
         job: "UI/UX designer",
-        image: "https://images.unsplash.com/photo-1537511446984-935f663eb1f4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+        image: "images/Agent-2.png",
+        // image: "https://images.unsplash.com/photo-1537511446984-935f663eb1f4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
         message: "Lorem ipsum, beatae illum non quae fugiat facilis aut modi nihil, maiores inventore laborum perferendis distinctio.Nisi aliquid repudiandae dolor sit amet consectetur adipisicing elit. Eaque aliquam cumque corrupti iure!"
     },
 
@@ -18,7 +20,8 @@ const clientReviews = [{
         id: 3,
         name: "Gwen Garza",
         job: "Baxk-end Web Developer",
-        image: "https://images.unsplash.com/photo-1464863979621-258859e62245?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=386&q=80",
+        image: "images/Agent-3.png",
+        // image: "https://images.unsplash.com/photo-1464863979621-258859e62245?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=386&q=80",
         message: "cumque corrupti beatae illum non quae fugiat facilis aut modi nihil, maiores inventore laborum perferendis Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque aliquam  distinctio.Nisi aliquid repudiandae iure!"
     },
 
@@ -26,7 +29,8 @@ const clientReviews = [{
         id: 4,
         name: "Kristi Larson",
         job: "React Developer",
-        image: "https://images.unsplash.com/photo-1556157382-97eda2d62296?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+        image: "images/Agent-4.png",
+        // image: "https://images.unsplash.com/photo-1556157382-97eda2d62296?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
         message: "Eaque aliquam cumque corrupti beatae illum non quae fugiat facilis aut modi nihil, maiores inventore laborum perferendis distinctio.Nisi aliquid repudiandae iure! Lorem ipsum, dolor sit amet consectetur adipisicing elit."
     },
 
@@ -34,7 +38,8 @@ const clientReviews = [{
         id: 5,
         name: "Pauline Mckenzie",
         job: "Node JS Developer",
-        image: "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+        image: "images/Agent-5.png",
+        // image: "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
         message: " nihil, maiores inventore laborum perferendis distinctio.Nisi aliquid repudiandae iure! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque aliquam cumque corrupti beatae illum non quae fugiat facilis aut modi"
     },
 
@@ -42,7 +47,8 @@ const clientReviews = [{
         id: 6,
         name: "Genevieve Edwards",
         job: "Graphic Designer",
-        image: "https://images.unsplash.com/photo-1543132220-c6440149c632?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDJ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+        image: "images/Agent-6.png",
+        // image: "https://images.unsplash.com/photo-1543132220-c6440149c632?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDJ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
         message: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque aliquam cumque corrupti beatae.Nisi aliquid repudiandae iure! illum non quae fugiat facilis aut modi nihil, maiores inventore laborum perferendis distinctio"
     },
 ];
