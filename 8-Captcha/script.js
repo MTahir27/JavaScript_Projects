894b6756f3ba26717e4ddf2a6976f568a858509a
Muhammad Tahir <m.tahiridrees27@gmail.com>
2021-12-05 09:59:25 +0500

    Tex Diduction

---
+++ 8-Captcha/script.js
@@ -0,0 +1,24 @@
+const amount = document.getElementById('amount');
+
+const submit = document.getElementById("submit");
+
+const result = document.getElementById("result");
+
+document.onsubmit = function (event) {
+    event.preventDefault();
+}
+
+
+submit.onclick = function () {
+    const value = amount.value;
+    console.log(value);
+
+    if (value < 50000) {
+        const tex = (value * 11) / 100;
+        result.innerText = "Your Total Amount After 11% text deduction is " + tex;
+
+    } else {
+        const tex = (value * 16) / 100;
+        result.innerText = "Your Total Amount After 16% text deduction is " + tex;
+    }
+}
