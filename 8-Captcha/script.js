9c5d7b7053c911635301bfc189013e7b534f55e5
Muhammad Tahir <m.tahiridrees27@gmail.com>
2021-12-05 21:44:19 +0500

    Captcha Code genderated & Print

--- 8-Captcha/script.js
+++ 8-Captcha/script.js
@@ -1,13 +1,13 @@
+const captcahCode = document.querySelector('.captcha-token');
 const captcha = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-console.log(captcha);
-
-let captcahValue;
+let captcahValue = '';
 
 for (let i = 0; i <= 5; i++) {
     const captchaDigit = Math.floor(Math.random() * captcha.length);
-    captcahValue = captchaDigit;
-    captcahValue = captcha[captcahValue];
-
+    // captcahValue = captchaDigit;
+    // console.log(captcha[captcahValue]);
+    captcahValue = captcahValue + captcha[captchaDigit];
 }
-console.log(captcahValue);
+
+captcahCode.innerText = captcahCode.innerText + captcahValue;
