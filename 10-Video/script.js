9e88a1ef1d5ae02c7f452cf7830c2d9839221bb6
Muhammad Tahir <m.tahiridrees27@gmail.com>
2021-12-08 01:20:54 +0500

    Preloader Added

--- 10-Video/script.js
+++ 10-Video/script.js
@@ -1,6 +1,11 @@
+const preloader = document.querySelector(".preloader");
 const video = document.querySelector('.video-section');
 const videoBtn = document.querySelector('.video-btns');
-
+// Loader
+window.onload = function () {
+    preloader.classList.add("hide-preloader")
+}
+// BG Video
 videoBtn.onclick = function () {
     if (videoBtn.classList.contains('stop')) {
         videoBtn.classList.remove('stop');
