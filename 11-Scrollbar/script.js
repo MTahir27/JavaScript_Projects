9bff92082f1c067472a1b6f94860650366ea767b
Muhammad Tahir <m.tahiridrees27@gmail.com>
2021-12-10 22:28:39 +0500

    Hide navbar on tablet

--- 11-Scrollbar/script.js
+++ 11-Scrollbar/script.js
@@ -1,3 +1,6 @@
 // Get Current Year
 const date = document.getElementById("date");
 date.innerHTML = new Date().getFullYear();
+
+// Show Navbar on Scroll
+const navbar = document.getElementById("navbar");
