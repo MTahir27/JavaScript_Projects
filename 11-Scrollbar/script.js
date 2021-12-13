e82f2fa1249a658b56629ac47220a8d23ce73305
Muhammad Tahir <m.tahiridrees27@gmail.com>
2021-12-13 23:23:46 +0500

    Navbar Functionality

--- 11-Scrollbar/script.js
+++ 11-Scrollbar/script.js
@@ -2,5 +2,17 @@
 const date = document.getElementById("date");
 date.innerHTML = new Date().getFullYear();
 
-// Show Navbar on Scroll
-const navbar = document.getElementById("navbar");
+// Hide & Show Navbar
+const toggleBtn = document.querySelector('.toggle-btn');
+const collaspe = document.querySelector('.collapse');
+const navbarNav = document.querySelector('.navbar-nav');
+
+toggleBtn.onclick = function () {
+    const collaspeHeight = collaspe.getBoundingClientRect().height;
+    const navbarHeight = navbarNav.getBoundingClientRect().height;
+    if (collaspeHeight === 0) {
+        collaspe.style.height = `${navbarHeight}px`;
+    } else {
+        collaspe.style.height = `0px`;
+    }
+};
