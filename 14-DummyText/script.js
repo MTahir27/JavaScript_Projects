cb11731175e153ef5ee2420ae09049c239083c5c
Muhammad Tahir <m.tahiridrees27@gmail.com>
2021-12-13 23:48:57 +0500

    Disable Reload

---
+++ 14-DummyText/script.js
@@ -0,0 +1,9 @@
+// const form = document.querySelector("form")
+const amount = document.getElementById('amount');
+const generate = document.getElementById("generate");
+const textSection = document.querySelector('text-section');
+
+generate.onclick = (e) => {
+    e.preventDefault();
+
+}
