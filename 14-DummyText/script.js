68557378c2b2c758357f01d22d3a995fee465597
Muhammad Tahir <m.tahiridrees27@gmail.com>
2021-12-14 00:03:50 +0500

    Create Random Text on null value

--- 14-DummyText/script.js
+++ 14-DummyText/script.js
@@ -1,7 +1,7 @@
 // const form = document.querySelector("form")
 const amount = document.getElementById('amount');
 const generate = document.getElementById("generate");
-const textSection = document.querySelector('text-section');
+const textSection = document.querySelector('.text-section');
 
 const dummyText = [
     "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
@@ -29,6 +29,10 @@ generate.onclick = (e) => {
     e.preventDefault();
 
     const paragraphAmount = parseInt(amount.value);
-    console.log(paragraphAmount);
+    const randomNumber = Math.floor(Math.random() * dummyText.length);
+
+    if (isNaN(paragraphAmount) || paragraphAmount < 1 || paragraphAmount > 9) {
+        textSection.innerHTML = `<p>${dummyText[randomNumber]}</p>`;
+    }
 
 }
