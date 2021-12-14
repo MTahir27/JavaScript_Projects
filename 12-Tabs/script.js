7da785f021a06a96812f0ca8ff24c788b94d197d
Muhammad Tahir <m.tahiridrees27@gmail.com>
2021-12-14 21:51:55 +0500

    Get All Constants

---
+++ 12-Tabs/script.js
@@ -0,0 +1,7 @@
+// Get All Constants
+const tabSection = document.querySelector(".tab-container");
+console.log(tabSection);
+const tabBtns = document.querySelectorAll(".btns");
+console.log(tabBtns);
+const tabContent = document.querySelectorAll(".tab-data");
+console.log(tabContent);
