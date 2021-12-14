7d7bffd2da524f7df5bac8dc23633e573c250b01
Muhammad Tahir <m.tahiridrees27@gmail.com>
2021-12-14 22:02:27 +0500

    Move Active on Click Button

--- 12-Tabs/script.js
+++ 12-Tabs/script.js
@@ -1,7 +1,15 @@
 // Get All Constants
 const tabSection = document.querySelector(".tab-container");
-console.log(tabSection);
 const tabBtns = document.querySelectorAll(".btns");
-console.log(tabBtns);
 const tabContent = document.querySelectorAll(".tab-data");
-console.log(tabContent);
+
+// Select Id on Click
+tabSection.onclick = (e) => {
+    const tabId = e.target.dataset.id;
+    if (tabId) {
+        tabBtns.forEach((tabBtn) => {
+            tabBtn.classList.remove("active");
+            e.target.classList.add("active");
+        });
+    }
+}
