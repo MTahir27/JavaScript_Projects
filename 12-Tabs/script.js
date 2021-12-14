0702850d9f4cb372ad3c79cfe5c65c38563504fe
Muhammad Tahir <m.tahiridrees27@gmail.com>
2021-12-14 22:09:19 +0500

    Comment Added

--- 12-Tabs/script.js
+++ 12-Tabs/script.js
@@ -7,11 +7,14 @@ const tabContents = document.querySelectorAll(".tab-data");
 tabSection.onclick = (e) => {
     const tabId = e.target.dataset.id;
     if (tabId) {
+        // Add or Remove Active Class
         tabBtns.forEach((tabBtn) => {
             tabBtn.classList.remove("active");
             e.target.classList.add("active");
         });
+        // Get Id of Click Button
         const tabIds = document.getElementById(tabId);
+        // Remove Active Class & Add Active Class of Tab that id Match
         tabContents.forEach((tabContent) => {
             tabContent.classList.remove("active");
             tabIds.classList.add("active");
