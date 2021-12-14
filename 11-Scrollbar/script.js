da9c5ef3aa6692c0550af491df685747457df1cc
Muhammad Tahir <m.tahiridrees27@gmail.com>
2021-12-14 22:48:17 +0500

    Navbar Collaspe Styling

--- 11-Scrollbar/script.js
+++ 11-Scrollbar/script.js
@@ -7,6 +7,14 @@ const toggleBtn = document.querySelector('.toggle-btn');
 const collaspe = document.querySelector('.collapse');
 const navbarNav = document.querySelector('.navbar-nav');
 
+// Onresize Check Height
+window.onresize = () => {
+    width = window.innerWidth;
+    if (width < 775.98) {
+        collaspe.style.height = `0px`;
+    }
+}
+
 toggleBtn.onclick = function () {
     const collaspeHeight = collaspe.getBoundingClientRect().height;
     const navbarHeight = navbarNav.getBoundingClientRect().height;
