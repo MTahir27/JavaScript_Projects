b41dfd2abe6e0d4fd1f87ed7fc5282af0370139f
Muhammad Tahir <m.tahiridrees27@gmail.com>
2021-12-15 21:59:12 +0500

    Add zero in date & Minutes from (0 to 9)

--- 13-Countdown/script.js
+++ 13-Countdown/script.js
@@ -9,19 +9,25 @@ const endDate = document.querySelector('.end-time');
 const timer = document.querySelectorAll('.date h1');
 console.log(timer);
 
-const saleDate = new Date(2021, 27, 03, 12, 00, 00);
+const saleDate = new Date(2021, 27, 27, 12, 1, 00);
 // Get Year
 const year = saleDate.getFullYear();
 // Get Month
 const month = months[saleDate.getMonth()];
 // Get Date
-const date = saleDate.getDate();
+let date = saleDate.getDate();
+if (date <= 9) {
+    date = "0" + date;
+}
 // Get Day
 const day = dayName[saleDate.getDay()];
 // Get Hour
 const hour = saleDate.getHours();
 // Get Minutes
-const minutes = saleDate.getMinutes();
+let minutes = saleDate.getMinutes();
+if (minutes <= 9) {
+    minutes = "0" + minutes;
+}
 
 
 endDate.innerHTML = `End on ${day} ${date} ${month} ${year} , ${hour}:${minutes} AM`;
