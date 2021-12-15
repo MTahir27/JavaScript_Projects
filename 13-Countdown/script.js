561bbf1ed5f940b00c989f9a621d0264ed27e57f
Muhammad Tahir <m.tahiridrees27@gmail.com>
2021-12-15 21:54:46 +0500

    Show Month Name & Day Name

--- 13-Countdown/script.js
+++ 13-Countdown/script.js
@@ -1,3 +1,10 @@
+const months = [
+    "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
+];
+
+const dayName = [
+    "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"
+];
 const endDate = document.querySelector('.end-time');
 const timer = document.querySelectorAll('.date h1');
 console.log(timer);
@@ -6,15 +13,15 @@ const saleDate = new Date(2021, 27, 03, 12, 00, 00);
 // Get Year
 const year = saleDate.getFullYear();
 // Get Month
-const month = saleDate.getMonth();
+const month = months[saleDate.getMonth()];
 // Get Date
 const date = saleDate.getDate();
 // Get Day
-const day = saleDate.getDay();
+const day = dayName[saleDate.getDay()];
 // Get Hour
 const hour = saleDate.getHours();
 // Get Minutes
 const minutes = saleDate.getMinutes();
 
 
-endDate.innerHTML = `End on ${day} ${year} ${month}, ${date}, ${hour}:${minutes} AM`;
+endDate.innerHTML = `End on ${day} ${date} ${month} ${year} , ${hour}:${minutes} AM`;
