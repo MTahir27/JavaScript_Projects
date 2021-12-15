141d8568fd70cf22df069fd0cc7a1e919b46cec7
Muhammad Tahir <m.tahiridrees27@gmail.com>
2021-12-15 21:43:32 +0500

    Call Date Tag & Get Sale Date

---
+++ 13-Countdown/script.js
@@ -0,0 +1,5 @@
+const endDate = document.querySelector('.end-time');
+const timer = document.querySelectorAll('.date h1');
+console.log(timer);
+
+const saleDate = new Date(2021, 27, 03, 12, 00, 00);
