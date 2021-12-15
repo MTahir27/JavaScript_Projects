c1022f4538df399fe87c51b42199ce384a3cd3f9
Muhammad Tahir <m.tahiridrees27@gmail.com>
2021-12-15 21:48:54 +0500

    Get Date & Time Value & Show

--- 13-Countdown/script.js
+++ 13-Countdown/script.js
@@ -3,3 +3,18 @@ const timer = document.querySelectorAll('.date h1');
 console.log(timer);
 
 const saleDate = new Date(2021, 27, 03, 12, 00, 00);
+// Get Year
+const year = saleDate.getFullYear();
+// Get Month
+const month = saleDate.getMonth();
+// Get Date
+const date = saleDate.getDate();
+// Get Day
+const day = saleDate.getDay();
+// Get Hour
+const hour = saleDate.getHours();
+// Get Minutes
+const minutes = saleDate.getMinutes();
+
+
+endDate.innerHTML = `End on ${day} ${year} ${month}, ${date}, ${hour}:${minutes} AM`;
