652a82c1e10e6aaf8eb09cf33578c1549a40ed10
Muhammad Tahir <m.tahiridrees27@gmail.com>
2021-12-16 22:50:46 +0500

    Slide Behaviour Value

--- 16-Image_Slider/script.js
+++ 16-Image_Slider/script.js
@@ -3,9 +3,25 @@ console.log(slides);
 const prevbtn = document.querySelector(".prevBtn");
 const nextbtn = document.querySelector(".nextBtn");
 
+// Functionality Btn
+const defaultBtn = document.querySelector(".default");
+const loop = document.querySelector(".loop");
+
+
 slides.forEach((slide, index) => {
     slide.style.left = `${index*100}%`;
 });
+
+// Slide Behaviour
+let behaviour = 0;
+defaultBtn.onclick = () => {
+    behaviour = 0;
+    console.log(behaviour);
+}
+loop.onclick = () => {
+    behaviour = 1;
+    console.log(behaviour);
+}
 let count = 0;
 nextbtn.onclick = () => {
     if (count < slides.length - 1) {
