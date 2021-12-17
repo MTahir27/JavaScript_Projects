dc6d9568f7c3550cc0627ea752d53cfbbf727da6
Muhammad Tahir <m.tahiridrees27@gmail.com>
2021-12-18 00:00:51 +0500

    Show Alert & Remove alert

--- 15-Todo_List/script.js
+++ 15-Todo_List/script.js
@@ -4,3 +4,31 @@ const todoForm = document.querySelector(".todo-form");
 const todoInput = document.querySelector(".input-list");
 const submitBtn = document.querySelector('.submit-btn');
 const clear = document.querySelector(".clear");
+
+// OnSubmit Function
+todoForm.onsubmit = (e) => {
+    e.preventDefault();
+    todoInputFunction();
+}
+const todoInputFunction = () => {
+    const todoValue = todoInput.value;
+    if (todoValue !== '') {
+        console.log("Value Exist");
+    } else {
+        // alert.innerText = "Please Enter value";
+        alertFunction("Please Enter Value", "danger");
+    }
+}
+
+// Alert Function
+const alertFunction = (message, type) => {
+    alert.innerText = message;
+    alert.classList.add(`alert-${type}`);
+    // Remove Alert After 1s
+    setTimeout(
+        () => {
+            alert.textContent = "";
+            alert.classList.remove(`alert-${type}`);
+        }, 1000
+    );
+}
