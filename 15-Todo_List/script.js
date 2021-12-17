c22ce3aea1e439f78cb8e739fd41c879c582e9bd
Muhammad Tahir <m.tahiridrees27@gmail.com>
2021-12-17 23:41:22 +0500

    Calling Elements

---
+++ 15-Todo_List/script.js
@@ -0,0 +1,6 @@
+// Calling Elements
+const alert = document.querySelector(".alert");
+const todoForm = document.querySelector(".todo-form");
+const todoInput = document.querySelector(".input-list");
+const submitBtn = document.querySelector('.submit-btn');
+const clear = document.querySelector(".clear");
